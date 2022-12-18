4fa08d1645e4942f450eceb99df9819cb477fa2c
malfarizit <trangganualfarizi@gmail.com>
2022-12-18 10:20:16 +0700

    first commit

---
+++ loginForm/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace loginForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.cmbline = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbmodel = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(89, 220);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(100, 20);
+            this.txtpass.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(111, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Employee ID";
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Location = new System.Drawing.Point(103, 264);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(75, 23);
+            this.btnlogin.TabIndex = 4;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // cmbline
+            // 
+            this.cmbline.FormattingEnabled = true;
+            this.cmbline.Items.AddRange(new object[] {
+            "L01",
+            "L02",
+            "L03",
+            "L04",
+            "L05",
+            "L06",
+            "L07",
+            "L08",
+            "L09",
+            "L10"});
+            this.cmbline.Location = new System.Drawing.Point(79, 122);
+            this.cmbline.Name = "cmbline";
+            this.cmbline.Size = new System.Drawing.Size(121, 21);
+            this.cmbline.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(116, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "SMT Line";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(115, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "SMT Model";
+            // 
+            // cmbmodel
+            // 
+            this.cmbmodel.FormattingEnabled = true;
+            this.cmbmodel.Items.AddRange(new object[] {
+            "OHC SYDNEY 2B-2M",
+            "OHC SYDNEY 2B-3M",
+            "OHC SYDNEY 1B-1M"});
+            this.cmbmodel.Location = new System.Drawing.Point(79, 170);
+            this.cmbmodel.Name = "cmbmodel";
+            this.cmbmodel.Size = new System.Drawing.Size(121, 21);
+            this.cmbmodel.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(130)))), ((int)(((byte)(121)))));
+            this.ClientSize = new System.Drawing.Size(278, 333);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbmodel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbline);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtpass);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.ComboBox cmbline;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbmodel;
+    }
+}
+
---
+++ loginForm/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Threading;
+
+namespace loginForm
+{
+    public partial class Form1 : Form
+    {
+        public static string user;
+        MySqlCommand cmd;
+        MySqlDataAdapter ad;
+        MySqlConnection con = new MySqlConnection(@"server=127.0.0.1;uid=root;pwd=;database=login");
+        DataTable dt = new DataTable();
+        Thread th;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void openform()
+        {
+            Application.Run(new Form2());
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            if (cmbline.Text == "")
+            {
+                MessageBox.Show("Please Select Line!");
+            }
+            else if (txtpass.Text == "")
+            {
+                MessageBox.Show("Please Enter Password!");
+            }
+            else if (cmbmodel.Text == "")
+            {
+                MessageBox.Show("Please Select Model!");
+            }
+            else
+            {
+                con.Open();
+                cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                //cmd.CommandText = "Select username,password from admin_login WHERE username =@username AND password =@password";
+                cmd.CommandText = "Select * from admin_login WHERE username='" + txtpass.Text + "'";
+                //cmd.Parameters.AddWithValue("@username", txtusername.Text);
+                //cmd.Parameters.AddWithValue("@password", txtpass.Text);
+                cmd.ExecuteNonQuery();
+                ad = new MySqlDataAdapter(cmd);
+                ad.Fill(dt);
+                con.Close();
+                if (dt.Rows.Count == 1)
+                {
+                    //insertintoactivitylog();
+                    //Admin_ID = txtusername.Text;
+                    user = txtpass.Text;
+                    this.Close();
+                    th = new Thread(openform);
+                    th.SetApartmentState(ApartmentState.STA);
+                    th.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Username or Password!");
+                }
+            }
+        }
+    }
+}
